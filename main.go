e7721dab7ff9776e49ecfda95a8f8bb7866d9f6d
abdullah harits <harits.abdullah19@gmail.com>
2022-05-06 19:59:05 +0700

    add docker file

--- main.go
+++ main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"jobapp.com/m/applicant"
-	"jobapp.com/m/common"
+	_"jobapp.com/m/applicant"
+	_"jobapp.com/m/common"
 
 	"net/http"
 )
 
 func main() {
 
-	common.InitMongoDB()
+	// common.InitMongoDB()
 
 	routerLocal := gin.Default()
-	applicantGroup := routerLocal.Group("applicant")
-	applicant.ApplicantRouter(applicantGroup)
+	// applicantGroup := routerLocal.Group("applicant")
+	// applicant.ApplicantRouter(applicantGroup)
 
 	routerLocal.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "pong"})
